29f1da6d2468fc91cb60e02358bb527e45170a35
Zac Koop <zacmanshredz@gmail.com>
2017-01-15 15:52:08 -0800

    Lab 1 WebApp output complete.

--- index.php
+++ index.php
@@ -7,14 +7,19 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Student Records</title>
     </head>
     <body>
+        <header>
+            <h1>Student Records</h1>
+        </header>
         <?php
         include('Student.php');
-
-        $students = array();
         
+        $count = 1;  // used to number the students.
+        $students = array();  //array to hold the student records.
+        
+        //student 1
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,7 +30,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
         
-        
+        //student 2
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -36,9 +41,26 @@ and open the template in the editor.
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;
+        
+        //student 3
+        $third = new Student();
+        $third->surname = "Koop";
+        $third->first_name = "Zachary";
+        $third->add_email('school','zkoop@my.bcit.ca');
+        $third->add_grade(99);
+        $third->add_grade(76);
+        $third->add_grade(59);
+        $students['k009'] = $third;
+        
+        
+        ksort($students); // one of the many sort functions
 
-        foreach($students as $student)
+        //Print out students.
+        foreach($students as $student) {
+            echo 'Student ' . $count;
             echo $student->toString();
+            $count++;
+        }
         ?>
     </body>
 </html>
