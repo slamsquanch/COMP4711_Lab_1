8be70998086f81219ca441d6c5bc555e1c225f31
Zac Koop <zacmanshredz@gmail.com>
2017-01-15 15:20:18 -0800

    Student class added

--- Student.php
+++ Student.php
@@ -14,6 +14,7 @@
 class Student {
     //put your code here
     
+    
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -21,11 +22,33 @@ class Student {
         $this->grades = array();
     }
     
+    
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
+    
+    
+    
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
+    
 }
