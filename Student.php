de94eade23ede2252c9800a49989a742b1245a9a
Zac Koop <zacmanshredz@gmail.com>
2017-01-15 15:50:02 -0800

    Students created and printed out.  Commented.

--- Student.php
+++ Student.php
@@ -22,18 +22,18 @@ class Student {
         $this->grades = array();
     }
     
-    
+    //Stores a student's email address.
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
-    
+    //Stores a student's grades.
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
     
-    
+    //Calculates the average grade for a student.
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -41,7 +41,7 @@ class Student {
         return $total / count($this->grades);
     }
     
-    
+    //Formats the output of a student's information. 
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
